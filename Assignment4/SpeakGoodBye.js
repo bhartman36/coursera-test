ec8e82725570fa65390f396ae4afc9e91daf5644
Brian Hartman <41649307+bhartman36@users.noreply.github.com>
2021-01-03 20:55:22 -0500

    Add files via upload

--- Assignment4/SpeakGoodBye.js
+++ Assignment4/SpeakGoodBye.js
@@ -8,7 +8,7 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-// var byeSpeaker =
+var byeSpeaker ={};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -17,7 +17,7 @@ var speakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 function speak(name) {
-  console.log(speakWord + " " + name);
+  console.log(byeSpeaker.speakWord + " " + name);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
