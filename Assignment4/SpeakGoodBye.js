cd09a5333690fdd3f0d352b2105cfd011aa3a00f
Brian Hartman <41649307+bhartman36@users.noreply.github.com>
2021-01-04 02:21:11 -0500

    Add files via upload

--- Assignment4/SpeakGoodBye.js
+++ Assignment4/SpeakGoodBye.js
@@ -18,7 +18,7 @@ console.log(speakWord);
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 function speak(names) {
-  console.log(speakWord + " " + names);
+  console.log(speakWord + " " + name);
 }{};
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
